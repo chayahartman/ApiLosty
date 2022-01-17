b4d80b5ca6d49b2c6f566008fc39f4cfa2cda57b
hadassaBenShalom <sh2130362@gmail.com>
2022-01-17 22:57:47 +0200

    trying

---
+++ BL/BL/UsersBL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace BL
+{
+    public class UsersBL
+    {
+        public int countUsers()
+        {
+            UsersDAL dl = new UsersDAL();
+            DataTable dt = dl.count();
+            if (dt.Rows.Count > 0)
+                return dt.Rows.Count;
+            else
+                return -1;
+
+        }
+    }
+}
