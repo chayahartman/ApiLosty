6e45370cf812ba1846b216e33ade3b66412b2a69
chayaHadasa <chaya.job8842@gmail.com>
2022-01-18 00:14:56 +0200

    new-api

--- ApiLosty/ApiLosty/Controllers/UsersController.cs
+++ ApiLosty/ApiLosty/Controllers/UsersController.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BL;
+using DAL;
 
 namespace ApiLosty.Controllers
 {
-    public class UsersController
+    public class UsersController : ApiController
     {
 
 
+        public HttpResponseMessage GET()
+         {
+            UsersBL bl = new UsersBL();
+            int count = bl.countUsers();
+            return Request.CreateResponse(HttpStatusCode.OK, count);
 
+        }
 
     }
 }
---
+++ BL/BL/UsersBL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Data;
+
+namespace BL
+{
+    public class UsersBL
+    {
+        public int countUsers()
+        {
+            UsersDL dl = new UsersDL();
+            DataTable dt = dl.count();
+            if (dt.Rows.Count > 0)
+                return dt.Rows.Count;
+            else
+                return -1;
+
+        }
+    }
+}
