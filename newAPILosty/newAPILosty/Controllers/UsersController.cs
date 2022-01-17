5973675d767733397bc64012dc0877296a6460a9
chayaHadasa <chaya.job8842@gmail.com>
2022-01-18 00:40:29 +0200

    please

---
+++ newAPILosty/newAPILosty/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BL;
+using DAL;
+
+namespace newAPILosty.Controllers
+{
+
+    public class UsersController : ApiController
+    {
+
+
+        public HttpResponseMessage GET()
+        {
+            UsersBL bl = new UsersBL();
+            int count = bl.countUsers();
+            return Request.CreateResponse(HttpStatusCode.OK, count);
+
+        }
+    }
+}
---
+++ newAPILosty/newAPILosty/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="newAPILosty.WebApiApplication" Language="C#" %>
