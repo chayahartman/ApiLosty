001c2c7b27bdac0a26f29446ff1acee9f6b9789b
hadassaBenShalom <sh2130362@gmail.com>
2022-01-17 23:29:17 +0200

    problem with API

--- ApiLosty/ApiLosty/Controllers/UsersController.cs
+++ ApiLosty/ApiLosty/Controllers/UsersController.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BL;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace ApiLosty.Controllers
 {
-    public class UsersController
-    {
+	public class UsersController
+	{
+        public class UserController : ApiController
+        {
 
+            public HttpResponseMessage GET()
+            {
+                UsersBL bl = new UsersBL();
+                int count = bl.countUsers();
+                return Request.CreateResponse(HttpStatusCode.OK, count);
 
+            }
 
-
+        }
     }
 }
